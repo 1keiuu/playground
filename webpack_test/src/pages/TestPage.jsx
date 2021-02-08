bd961e4075548fc7521deb45e707822e59db7e63
ikkei12 <ikkei12.h@gmail.com>
2021-02-08 10:11:07 +0900

    add html plugin

--- webpack_test/src/pages/TestPage.jsx
+++ webpack_test/src/pages/TestPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export default class TestPage extends React.Component {
     render() {
-      return <p>testです</p>;
+      return <p>testですa</p>;
     }
 }
--- webpack_test/webpack.config.js
+++ webpack_test/webpack.config.js
@@ -1,15 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
     another: "./src/another.js",
-    test: "./src/app.jsx",
+    app: "./src/app.jsx",
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist/src"),
   },
   module: {
     rules: [
@@ -22,4 +23,14 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "./index.html",
+    }),
+  ],
 };
