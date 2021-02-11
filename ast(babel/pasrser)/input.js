aec6903f308660468e744e741e962b7b16f47466
ikkei12 <ikkei12.h@gmail.com>
2021-02-11 20:55:55 +0900

    parse and generate

--- ast(babel/pasrser)/input.js
+++ ast(babel/pasrser)/input.js
@@ -1,3 +1,5 @@
 const str = "testです";
 
 console.log(str);
+
+console.log("っw");
--- ast(babel/pasrser)/parser.js
+++ ast(babel/pasrser)/parser.js
@@ -1,10 +1,21 @@
 const parser = require("@babel/parser");
 const util = require("util");
 const fs = require("fs");
+const generator = require("@babel/generator");
 
 const target = fs.readFileSync("./input.js", { encoding: "utf-8" });
-const data = util.inspect(parser.parse(target), false, null);
-fs.writeFile("./output.js", data, (err) => {
+const ast = parser.parse(target);
+
+for (let i = 0; i < ast.program.body.length; i++) {
+  const node = ast.program.body[i];
+  if (node.declarations) {
+    // 変数の中身をhogehogeに変換
+    node.declarations[0].init.value = "hogehoge";
+  }
+}
+
+const generatedCode = generator.default(ast, target);
+
+fs.writeFile("./output.js", generatedCode.code, (err) => {
   if (err) throw err;
-  console.log(err);
 });
