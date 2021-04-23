2225f0e7c4bf805d8a72f14328d3b7303334c02e
1keiuu <ikkei12.h@gmail.com>
2021-04-23 17:11:46 +0900

    add real world http

---
+++ go/rw_http/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println((string(dump)))
+	fmt.Fprintf(w, "<html><body>aaa</body></html>")
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	httpServer.Addr = ":8888"
+	log.Println(httpServer.ListenAndServe())
+}
